c108c8e20d3704e72545c25c12489987dcd3bf94
Deivixas <88595401+Deivixas@users.noreply.github.com>
2023-07-21 17:19:00 +0300

    added some new styles

--- src/components/CV.js
+++ src/components/CV.js
@@ -1,6 +1,6 @@
 import './CV.css';
-import Profile from '../src/assets/3.jpg';
-import Image from '../src/assets/1.png'
+import Profile from '../assets/3.jpg';
+import Image from '../assets/1.png'
 import {FaLinkedin} from 'react-icons/fa'
 import { SiGmail } from 'react-icons/si';
 import { BsTelephoneFill } from 'react-icons/bs';
@@ -13,14 +13,14 @@ function App() {
 <div className='header'>
   <img className='profile' src={Profile} alt="profile" /> 
   <h1>Deividas Skirgaila</h1>
-      <p>Front-end Developer</p>
+      <p>Web programuotojas</p>
 </div>
      <div className="cv-container"> 
     
       <div className="cv-column">
          
         <section>
-        <h2>Contacts</h2>
+        <h2>Kontaktai</h2>
         <ul className='contact'>
         <li>
        <FaLinkedin/> <a href='https://www.linkedin.com/in/deividas-skirgaila-7a6b00253/' target="_blank" rel="noopener noreferrer">LinkedIn</a>
@@ -43,22 +43,22 @@ function App() {
         </ul>
       </section>
       <section>
-            <h2>Education</h2>
+            <h2>Išsilavinimas</h2>
             <h3>CodeAcademy</h3>
-            <p>Front-end/intensive/480h-courses</p>
+            <p>Web/intensyvus/480h-kursai</p>
             <img className="photo" src={Image} alt="img" />
           </section>
 
       <section>
-            <h2>Skills</h2>
+            <h2>Įgūdžiai</h2>
             <ul>
               <li className="bold">React</li>
               <li className="bold">JavaScript</li>
               <li className="bold">HTML</li>
               <li className="bold">CSS</li>
-              <li>Responsive Web Design</li>
-              <li>Version Control (Git)</li>
-              <li>Problem solving</li>
+              <li>Dizainas pritaikytas visiem įrenginiam</li>
+              <li>Versijavimo sistema (Git)</li>
+              <li>Problemų sprendimas</li>
             </ul>
           </section>
          
@@ -68,31 +68,30 @@ function App() {
       
      
       <section >
-            <h2>About Me</h2>
-            <p >
-            As a passionate front-end developer, I excel in crafting user-friendly web applications using React, JavaScript, and CSS. With a proactive learning approach, I stay updated with the latest advancements in web development, enabling me to adapt swiftly and deliver innovative solutions. I thrive on pushing boundaries, creating memorable online experiences, and collaborating with forward-thinking teams. Let's create something exceptional together!
+            <h2>Apie mane</h2>
+            <p >           
+            Kaip aistringas priekinės dalies kūrėjas, man puikiai sekasi kurti patogias žiniatinklio programas naudojant React, JavaScript ir CSS. Taikydamas iniciatyvų mokymosi metodą, nuolat matau naujausius žiniatinklio kūrimo pasiekimus, todėl galiu greitai prisitaikyti ir pateikti naujoviškus sprendimus. Man sekasi peržengti ribas, kurti įsimintinas internetines patirtis ir bendradarbiauti su pažangiomis komandomis. Sukurkime ką nors išskirtinio kartu!
             </p>
           </section>
           <section >
-            <h2>Work Experience</h2>
-            <h3>Parcel Sorter</h3>
-            <p>DPD Lithuania</p>
+            <h2>Darbo patirtis</h2>
+            <h3>Siuntų rūšiavimas</h3>
+            <p>DPD Lietuva</p>
             <ul>
-              <li>Sorting parcels by regions, cities.</li>
-              <li>Unloading and loading parcels into trailers manually</li>
+              <li>Siuntų rūšiavimas pagal regionus, miestus.</li>
+              <li>Siuntų iškrovimas ir pakrovimas į priekabas rankiniu būdu</li>
               <li>
-                Unloading and loading of freight transport with an electric
-                forklift
+              Krovinio transporto iškrovimas ir pakrovimas elektriniu šakiniu krautuvu.
               </li>
-              <li>Filling out CMR documents.</li>
-              <li>Warehouse maintenance and organization.</li>
+              <li>CMR dokumentų pildymas.</li>
+              <li>Sandėlio priežiūra ir organizavimas.</li>
             </ul>
           </section> 
           <section >
-            <h2>Languages</h2>
+            <h2>Kalbos</h2>
             <ul>
-              <li>Lithuanian</li>
-              <li>English</li>
+              <li>Lietuvių</li>
+              <li>Anglų</li>
             </ul> 
             </section>
 
